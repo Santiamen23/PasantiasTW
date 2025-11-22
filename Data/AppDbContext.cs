4feda02e13d40d1428a9cf82144a81233d71bf41
Santiamen23 <santymenber@gmail.com>
2025-11-22 16:34:10 -0400

    feat: DbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,21 @@
-﻿namespace PasantiasTW.Data
+﻿using Microsoft.EntityFrameworkCore;
+using PasantiasTW.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace PasantiasTW.Data
 {
-    public class AppDbContext
+    public class AppDbContext:DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users => Set<User>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>();
+        }
     }
 }
