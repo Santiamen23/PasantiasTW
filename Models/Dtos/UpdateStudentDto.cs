aa942f936ba459bbace5d7a83089cbbb4ba0057c
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2025-11-24 23:44:22 -0400

    feat: adding StudentService in Services Folder

--- Models/Dtos/UpdateStudentDto.cs
+++ Models/Dtos/UpdateStudentDto.cs
@@ -2,9 +2,9 @@
 {
     public record UpdateStudentDto
     {
-        public string? Name { get; set; } 
-        public string? Email {  get; set; } 
-        public string? Carrera {  get; set; } 
-        public string? Phone { get; set; } 
+        public string Name { get; set; } 
+        public string Email {  get; set; } 
+        public string Carrera {  get; set; } 
+        public string Phone { get; set; } 
     }
 }
--- Repositories/IStudentRepository.cs
+++ Repositories/IStudentRepository.cs
@@ -5,7 +5,7 @@ namespace PasantiasTW.Repositories
     public interface IStudentRepository
     {
         Task<IEnumerable<Student>> GetAll();
-        Task<Student?> GetOne(Guid id);
+        Task<Student> GetOne(Guid id);
         Task Add(Student student);
         Task Update(Student student);
         Task Delete(Student student);
