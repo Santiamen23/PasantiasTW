61e65ae2d10a77bec3ba5bbc5bedfb65e4bd916a
JMjuanma <juan.soruco@ucb.edu.bo>
2025-11-25 23:09:14 -0400

    fix: secondUpdatePracticeModel

--- Models/Practice.cs
+++ Models/Practice.cs
@@ -1,14 +1,22 @@
 namespace PasantiasTW.Models
 {
+    public enum PracticeStatus
+    {
+        Pending,
+        Active,
+        Finished
+    }
+
     public class Practice
     {
-        public Guid PracticeId { get; set; }
+        public Guid PracticeId { get; set; } = Guid.NewGuid();
         public Guid StudentId { get; set; }
         public Guid CompanyId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
-        public string Status { get; set; } = string.Empty;
+        public PracticeStatus Status { get; set; } = PracticeStatus.Pending;
 
-        public Company Company = default;
+        public Company Company { get; set; } = default;
+        public Student Student { get; set; }
     }
 }
